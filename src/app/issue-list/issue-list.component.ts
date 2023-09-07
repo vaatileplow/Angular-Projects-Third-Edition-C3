550bfe5212a270bc4c447b374b8ac2f43be1a080
Michael Hance <h_michael52@hotmail.fr>
2023-09-07 05:18:07 +0200

    Add resolving issue dialog-box

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -10,6 +10,7 @@ import { IssuesService } from '../issues.service';
 export class IssueListComponent implements OnInit {
   issues: Issue[] = [];
   showReportIssue = false;
+  selectedIssue: Issue | null = null;
 
   constructor(private issueService: IssuesService) {}
 
@@ -24,5 +25,13 @@ export class IssueListComponent implements OnInit {
   onCloseReport() {
     this.showReportIssue = false;
     this.getIssues();
+  }
+
+  onConfirm(confirmed: boolean) {
+    if (confirmed && this.selectedIssue) {
+      this.issueService.completeIssue(this.selectedIssue);
+      this.getIssues();
+    }
+    this.selectedIssue = null;
   }
 }
